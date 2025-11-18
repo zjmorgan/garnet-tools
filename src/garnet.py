6da286d7488b377a573b5be175d6f2067f8f28ea
zjmorgan <zjmorgan@mtu.edu>
2025-11-18 15:48:42 -0500

    Revert changes

--- src/garnet.py
+++ src/garnet.py
@@ -2374,7 +2374,8 @@ class FormModel:
 
     def set_instrument(self, instrument):
         self.beamline = beamlines[instrument]
-        self.reduction.generate_plan(instrument)
+        if self.reduction.plan is None:
+            self.reduction.generate_plan(instrument)
 
     def get_processes(self):
         return self.beamline["Processes"]
